23bdd447ce8aafcba0bc7c3f2919183a96d17665
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2023-11-23 15:52:33 -0300

    feat: Criação de navbar, menu superior.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">ToDoList</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Tarefas</b-nav-item>
+          <b-nav-item href="#">Formulário</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
